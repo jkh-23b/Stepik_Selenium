ab1bf0fb200610c42fc95235d5948e0c5d653883
JKH_23B <khabibullaev54@gmail.com>
2023-03-02 17:26:32 +0500

    stepik

--- Oformleniye_testov_v_stile_unittest.py
+++ Oformleniye_testov_v_stile_unittest.py
@@ -1,11 +1,16 @@
 import unittest
 
-class TestSum(unittest.TestCase):
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+from my_sum import sum
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
 
 if __name__ == '__main__':
     unittest.main()
