0f458489ce75a0c0089fce70fc80d1abeea3a10d
JKH_23B <khabibullaev54@gmail.com>
2023-03-15 14:15:53 +0500

    done

--- unittest.py
+++ unittest.py
@@ -17,8 +17,30 @@ class test_class_name(unittest.TestCase):
         driver.find_element(By.CSS_SELECTOR, ".second_block input.first").send_keys("+998991234567")
         driver.find_element(By.CSS_SELECTOR, ".second_block input.second").send_keys("Uzbekistan")
         driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
-        time.sleep(2)
+        time.sleep(1)
 
-        self.assertEqual()
+        text_welc = driver.find_element(By.TAG_NAME, "h1").text
+        needed_text = "Congratulations! You have successfully registered!"
+        self.assertEqual(text_welc, needed_text, 'No text')
+
+
+    def test_2(self):
+        driver.get("https://suninjuly.github.io/registration2.html")
+        time.sleep(1)
+
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.first").send_keys("Jasur")
+        driver.find_element(By.CSS_SELECTOR, ".first_block input.third").send_keys("testt@gmail.com")
+        driver.find_element(By.CSS_SELECTOR, ".second_block input.first").send_keys("+998991112233")
+        driver.find_element(By.CSS_SELECTOR, ".second_block input.second").send_keys("Uzbekistan")
+        driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
+        time.sleep(1)
+
+        text_welc2 = driver.find_element(By.TAG_NAME, "h1").text
+        needed_text2 = "Congratulations! You have successfully registered!"
+        self.assertEqual(text_welc2, needed_text2, 'No text')
+
+if __name__ == "__main__":
+    unittest.main()
+    print("All passed")
 
 
